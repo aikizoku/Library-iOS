21c6d89c1e0efc1341e5f52fa891fdbb3acdcda9
yuki <yuki@thehero.jp>
2016-09-08 04:02:06 +0900

    update

---
+++ LibrarySample/LibrarySample/LibraryViewController.swift
@@ -0,0 +1,79 @@
+//
+//  LibraryViewController.swift
+//  LibrarySample
+//
+//  Created by yukithehero on 2016/09/08.
+//  Copyright © 2016年 yukithehero. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class LibraryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sections = Variable<[LibrarySection]>([])
+    
+    let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var sections = Array<LibrarySection>()
+        
+        do {
+            let section = LibrarySection(title: "NSDate")
+            
+            do {
+                section.rows.value.append(LibraryRow(title: "NSDate+Parse"))
+            }
+            
+            sections.append(section)
+        }
+        
+        self.sections.value = sections
+        
+        self.sections.asObservable().subscribe(onNext: { [unowned self] (sections) in
+            self.tableView.reloadData()
+        }).addDisposableTo(disposeBag)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.sections.value.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.sections.value[section].rows.value.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        if let cell = cell {
+            if let textLabel = cell.textLabel {
+                textLabel.text = self.sections.value[indexPath.section].rows.value[indexPath.row].title
+            }
+        }
+        return cell ?? UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+    }
+}
+
+class LibrarySection: AnyObject {
+    
+    var title = ""
+    var rows = Variable<[LibraryRow]>([])
+    
+    init(title: String) {
+        self.title = title
+    }
+}
+
+class LibraryRow: AnyObject {
+    
+    var title = ""
+    
+    init(title: String) {
+        self.title = title
+    }
+}
+
---
+++ LibrarySample/LibrarySample/MethodViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MethodViewController.swift
+//  LibrarySample
+//
+//  Created by yukithehero on 2016/09/08.
+//  Copyright © 2016年 yukithehero. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class MethodViewController: UIViewController {
+
+    @IBOutlet weak var stageView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+}
