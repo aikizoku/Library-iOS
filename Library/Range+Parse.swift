fe1bdc85389cdd1e8a6c0dd89576214e99b12eb0
Yuki Hirose <yuki.hirose@dena.com>
2016-09-12 20:55:32 +0900

    update

---
+++ Library/Range+Parse.swift
@@ -0,0 +1,18 @@
+//
+//  Range+Parse.swift
+//  moonshot
+//
+//  Created by Yuki Hirose on 2016/09/12.
+//  Copyright © 2016年 Yuki Hirose. All rights reserved.
+//
+
+import Foundation
+
+extension Range {
+    
+    func toNSRange() -> NSRange {
+        let s = Int(String(self.startIndex))!
+        let e = Int(String(self.endIndex))!
+        return NSMakeRange(s, e - s)
+    }
+}
