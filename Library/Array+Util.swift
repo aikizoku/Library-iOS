d8f0a6a25799802307dc49dc2a4bd21bdfabefb4
yuki <yuki@thehero.jp>
2016-09-16 02:28:12 +0900

    update

---
+++ Library/Array+Util.swift
@@ -0,0 +1,21 @@
+//
+//  Array+Util.swift
+//  LibrarySample
+//
+//  Created by yukithehero on 2016/09/16.
+//  Copyright © 2016年 yukithehero. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    /**
+     最後のIndexを取得する
+     */
+    var lastIndex: Int {
+        get {
+            return count == 0 ? 0 : count - 1
+        }
+    }
+}
