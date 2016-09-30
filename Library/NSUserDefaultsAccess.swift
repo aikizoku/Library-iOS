e92e817ef7b126e8f79fa06e2119ac368f65835e
Yuki Hirose <yuki.hirose@dena.com>
2016-09-30 12:30:27 +0900

    update

--- Library/NSUserDefaultsAccess.swift
+++ Library/NSUserDefaultsAccess.swift
@@ -14,10 +14,50 @@ class NSUserDefaultsAccess: NSObject {
     override init() {
         super.init()
         
-        
+        registerDefaults()
+        setupProperty()
+        addObserver()
+    }
+    
+    deinit {
+        removeObserver()
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if let keyPath = keyPath {
+            userDefaults.setObject(change?["new"], forKey: keyPath)
+            userDefaults.synchronize()
+        }
+    }
+    
+    private func registerDefaults() {
+        let dic = propertyNames.reduce([String:AnyObject]()) { (dic, key) -> [String:AnyObject] in
+            var dic = dic
+            dic[key] = valueForKey(key)
+            return dic
+        }
+        userDefaults.registerDefaults(dic)
+    }
+    
+    private func setupProperty() {
+        propertyNames.forEach {
+            setValue(userDefaults.objectForKey($0), forKey: $0)
+        }
+    }
+    
+    private func addObserver() {
+        propertyNames.forEach {
+            addObserver(self, forKeyPath: $0, options: .New, context: nil)
+        }
+    }
+    
+    private func removeObserver() {
+        propertyNames.forEach {
+            removeObserver(self, forKeyPath: $0)
+        }
     }
     
     private var propertyNames: [String] {
-        return Mirror(reflecting: self).children.flatMap { $0.label }
+        return Mirror(reflecting: self).children.flatMap({ $0.label })
     }
 }
